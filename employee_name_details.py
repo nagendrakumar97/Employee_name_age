677e29565b608f1613f2850be5558f2e11c48318
SATYA NAGENDRA KUMAR <nagendrakumarsurisetty@gmail.com>
2021-03-25 15:25:00 +0530

    Update employee_name_details.py

--- employee_name_details.py
+++ employee_name_details.py
@@ -1,9 +1,21 @@
 l=list()
+i=1
 while True:
+    print("\n","-"*100)
+    print("*********Enter Employee {0} Details*********".format(i))
     try:
-        details = {'name':str(input("enter your name")), 'Age': int(input("Enter your age"))}
-        l.append(details)
+        l.append({'name':str(input("Enter your name: ")), 'Age': int(input("Enter your age: "))})
+        i+=1
+    except ValueError as x :
+        print(x)
+    
+    if(input("\nTo stop Press 'q' else press any other key")== 'q'):
         print(l)
+        break
 
-    except ValueError as x:
-        print(x,"\nenter again" )
+print("\nNAME\t\t\t\tAGE\t\t\t")
+for x in l:
+    print("{0}\t\t\t\t{1}".format(x['name'],x['Age']))
+
+
+   
